c5fa3a01c2e3fdf26400a702bacbab1070590321
Osterholz, Clinton <Clinton.Osterholz@convertro.com>
2018-08-29 17:01:53 -0400

    scribblepad.py changes

--- scribblepad.py
+++ scribblepad.py
@@ -1,1 +1,17 @@
 #Write a function that reads the file words.txt and builds a list with one element per word. Write two versions of this function, one using the append method and the other using the idiom t = t + [x]. Which one takes longer to run? Why?
+
+def word_list():
+    x = []
+    fin = open('words.txt')
+    for line in fin:
+        x.append(line.strip())
+    print x
+
+def word_lister():
+    x = []
+    fin = open('words.txt')
+    for line in fin:
+        x = x + [line.strip()]
+    print x
+
+print word_list()
